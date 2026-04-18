b739a93eecece0942f8adbca2c8b16d1e2ad63de
ThisIsAName <senioreysey@163.com>
2026-04-19 00:26:15 +0800

    Add SmilingPC.java to display a smiling face

---
+++ Week01/SmilingPC.java
@@ -0,0 +1,15 @@
+public class SmilingPC {
+    public static void main(String[] args) {
+      System.out.println("  _________________");
+      System.out.println(" |   ___________   |");
+      System.out.println(" |  |   0   0   |  |");
+      System.out.println(" |  |  )  _  (  |  |");
+      System.out.println(" |  |   \\___/   |  |");
+      System.out.println(" |  |___________|  |");
+      System.out.println(" |______|\\_/|______|");
+      System.out.println("   __|__|/ \\|__|__");
+      System.out.println("  // *********** \\\\");
+      System.out.println(" // ************* \\\\");
+      System.out.println("---------------------");
+  }
+}
