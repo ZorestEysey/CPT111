e32b489b7b3ea11dc0eeca61c40d30f6c8833e01
ThisIsAName <senioreysey@163.com>
2026-04-19 00:24:59 +0800

    Add SimpleMath class for basic arithmetic operations

---
+++ Week01/SimpleMath.java
@@ -0,0 +1,12 @@
+public class SimpleMath {
+    public static void main(String[] args)  {
+        int num_1=10;
+        int num_2 = 3;
+        System.out.println("num1 + num2 = "+(num_1+num_2));
+        System.out.println("num1 / num2 = "+(num_1/num_2));
+        double real_1=10.0;
+        double real_2=3.0;
+        System.out.println("real1 + real2 = "+ (real_1+real_2));
+        System.out.println("real1 / real2 = "+ (real_1/real_2));
+    }
+}
