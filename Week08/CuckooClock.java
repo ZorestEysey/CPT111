48a6f5cab5940ef96560087cfa6d64c2878e941a
ThisIsAName <senioreysey@163.com>
2026-04-22 22:08:22 +0800

    Clean up CuckooClock.java by removing comments
    Removed comments and whitespace from CuckooClock class.

--- Week08/CuckooClock.java
+++ Week08/CuckooClock.java
@@ -1,22 +1,9 @@
-/*
- * Exercise Week #8 */
 public class CuckooClock extends Clock {
 
-
-    // Exercise #8.4  
-    // Creates a Cuckoo clock whose initial time is h hours and m minutes.
     public CuckooClock(int h, int m) {
         super(h,m);
     }
-
-
-    // Exercise #8.5  
-    // Adds 1 minute to the time on this Cuckoo clock.
-    // In addition,  it prints "Cuckoo!" at the start of every hour
-    // It prints one time for each hour
-    // Whether it is morning or night does not change the number of times it prints
-    // For example, for 14:00, it prints "Cuckoo!" two times;
-    //              and for 00:00 and 12:00, it prints "Cuckoo!" twelve times.
+    
     @Override
     public void tick() {
         super.tick();
@@ -27,8 +14,6 @@ public class CuckooClock extends Clock {
         }
     }
 
-
-    // Test Client  
     public static void main(String[] args) {
         CuckooClock cc1 = new CuckooClock(0, 58);
         cc1.tick();
